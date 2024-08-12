f8cc7841bb52ec9d21f1fa545927535b9988b974
Banele Ngubeni <banelengubeni5@gmail.com>
2024-08-12 11:56:10 +0200

    Add files via upload

---
+++ Data Cleaning in MySQL.sql
@@ -0,0 +1,125 @@
+select *
+from layoffs;
+
+create table layoffs_staging
+like layoffs;
+
+select*
+from layoffs_staging;
+
+Insert layoffs_staging
+select *
+from layoffs;
+with duplicate_cte as 
+(
+select*,
+row_number() over (partition by company,location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num
+from layoffs_staging)
+select *
+from duplicate_cte
+where row_num > 1 ;
+
+
+
+
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select*
+from layoffs_staging2;
+
+insert into layoffs_staging2
+select *,
+row_number() over (partition by company,location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num
+from layoffs_staging;
+
+delete
+from layoffs_staging2
+where row_num > 1;
+
+select*
+from layoffs_staging2;
+
+select company, trim(company)
+from layoffs_staging2;
+
+UPDATE layoffs_staging2
+set company = trim(company);
+
+
+select  distinct industry
+from layoffs_staging2
+;
+
+update layoffs_staging2
+set industry = 'crypto'
+where industry = 'crypto%';
+
+select distinct country, trim(trailing '_' from country)
+from layoffs_staging2
+order by 1 ;
+
+update  layoffs_staging2
+set country = trim(trailing '_' from country)
+where country like 'united states%';
+
+
+select `date`,
+str_to_date( `date`, '%m/%d/%Y')
+from layoffs_staging2;
+
+update layoffs_staging2
+SET `date` = str_to_date(`date`, '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column `date` date;
+
+select*
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select*
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+on t1.company = t2.company
+where(t1.industry is null or t1.industry ='' )
+and t2.industry is not null;
+
+update layoffs_staging2
+set industry = null
+where industry = '';
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null 
+and t2.industry is not null;
+
+select*
+from layoffs_staging2
+where company ='airbnb'
+;
+delete
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select*
+from layoffs_staging2;
+
+alter table layoffs_staging2
+drop column row_num;
